58e6ca9d9bfee3ed4093c7c12b14177c37faa520
alexander-alxck <alexander.alexeichik28@gmail.com>
2026-09-18 20:58:50 +0300

    feat: add isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -198,8 +198,14 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 0; i < n; i += 1) {
+    if (n % i === 0 && i !== 1 && i !== n) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
